a22b9e452bac3cbaa7a7a326cc5c7288ee4595a4
antoinefa <fabien.antoine@m4x.org>
2026-05-22 21:16:52 -0400

    fix: scan chat-ui tailwind content

--- ui/tailwind.config.cjs
+++ ui/tailwind.config.cjs
@@ -1,5 +1,8 @@
 module.exports = {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
+  content: [
+    './src/**/*.{html,js,svelte,ts}',
+    '../packages/chat-ui/src/**/*.{html,js,svelte,ts}'
+  ],
   theme: {
     extend: {
       colors: {
