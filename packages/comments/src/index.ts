df7360f73e53bcf37641e224b5b54854cb8f3a99
antoinefa <fabien.antoine@m4x.org>
2026-06-02 18:00:35 -0400

    feat(BR-42c): add CommentStore port, pure-TS guards and package index

---
+++ packages/comments/src/index.ts
@@ -0,0 +1,43 @@
+export type {
+  Comment,
+  CommentAuthor,
+  CommentProvenance,
+  CommentState,
+  CommentTarget,
+  CommentTargetKind,
+  CommentThreadSummary,
+  LiveCommentTarget,
+  NewComment,
+  TargetQuery,
+} from './types.js';
+export {
+  LIVE_RECORD_CONTEXT_TYPES,
+  targetFromLive,
+  targetToLive,
+} from './types.js';
+
+export type {
+  CommentEvent,
+  CommentEventEnvelope,
+  CommentEventSink,
+  CommentEventType,
+} from './events.js';
+export {
+  CollectingCommentEventSink,
+  noopCommentEventSink,
+  toEventEnvelope,
+} from './events.js';
+
+export {
+  isComment,
+  isCommentAuthor,
+  isCommentEvent,
+  isCommentEventType,
+  isCommentState,
+  isCommentTarget,
+  isCommentTargetKind,
+  isCommentThreadSummary,
+} from './guards.js';
+
+export type { CommentStore } from './store.js';
+export { CommentNotFoundError, ThreadNotFoundError } from './store.js';
