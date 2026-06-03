8da475157f63135ed8004417bb96cd5f2170eb21
antoinefa <fabien.antoine@m4x.org>
2026-06-03 12:01:01 -0400

    feat(BR-39m): scaffold IdP screens static front (SvelteKit adapter-static)

---
+++ apps/auth-idp/web/tailwind.config.cjs
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    './src/**/*.{html,js,svelte,ts}',
+    '../../../packages/auth-ui/src/**/*.{html,js,svelte,ts}'
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: 'oklch(50% 0.134 242.749)',
+        accent: 'oklch(70.4% 0.14 182.503)',
+        warning: 'oklch(57.7% 0.245 27.325)'
+      }
+    }
+  },
+  plugins: []
+};
